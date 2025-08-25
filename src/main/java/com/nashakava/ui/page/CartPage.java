d876d26709f34e3cfee2d0cbec0a6ab11bf42874
HrushaNataliia <HrushaNataliia@users.noreply.github.com>
2025-08-25 08:56:42 +0000

    deploy: 374da9082a5242ebadceb97247b8115cc69212d0

--- src/main/java/com/nashakava/ui/page/CartPage.java
+++
@@ -1,9 +0,0 @@
-package com.nashakava.ui.page;
-
-import org.openqa.selenium.WebDriver;
-
-public class CartPage extends BasePage{
-    public CartPage(WebDriver driver) {
-        super(driver);
-    }
-}
